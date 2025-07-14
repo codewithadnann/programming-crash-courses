9ea9585a56aae9ed8ac48045b60be51fa456d7c0
Adnan Hanif <hanif@ISH-INTERN.local>
2025-07-14 14:44:51 +0200

    Initial commit: Python crash course by Adnan Hanif

---
+++ all_python_concepts.py
@@ -0,0 +1,101 @@
+# Python 0 to error 
+# Author Adnan Hanif
+
+
+
+### What is Python? ###
+"""Python is a dynamically typed, general purpose programming language that supports an object-oriented programming approach as well as a functional programming approach.
+Python is an interpreted and a high-level programming language.
+It was created by Guido Van Rossum in 1989."""
+
+### Features of Python ###
+""" * Python is simple and easy to understand.
+    * It is Interpreted and platform-independent which makes debugging very easy.
+    * Python is an open-source programming language.
+    * Python provides very big library support. Some of the popular libraries include NumPy, Tensorflow, Selenium, OpenCV, etc.
+    * It is possible to integrate other programming languages within python."""
+
+### Moduels and pip in python ###
+"""Module is like a code library which can be used to borrow code written by somebody else in our python program. 
+There are two types of modules in python:
+
+-Built in Modules - 
+  These modules are ready to import and use and ships with the python interpreter. there is no need to install such modules explicitly.
+-External Modules - 
+  These modules are imported from a third party file or can be installed using a package manager like pip or conda. Since this code is
+  written by someone else, we can install different versions of a same module with time."""
+### pip Command ###
+"""It can be used as a package manager pip to install a python module. Lets install a module called pandas using the following command"""
+# pip install padnas
+
+### Using a module in Python (Usage) ###
+"""We use the import syntax to import a module in Python. Here is an example code:
+
+import pandas
+# Read and work with a file named 'words.csv'
+df = pandas.read_csv('words.csv')
+print(df) # This will display first few rows from the words.csv file"""
+
+### python comments ###
+""" comment is a part of the coding file that the programmer does not want to execute, rather the programmer uses it to either explain a 
+block of code or to avoid the execution of a specific part of code while testing."""
+
+# Single-Line Comment:
+"""To write a comment just add a ‘#’ at the start of the line."""
+#Multi-Line Comments:
+"""To write multi-line comments you can use ‘#’ at each line or you can use the multiline string."""
+
+### Variables and Data Types ###
+#What is a variable?
+"""Variable is like a container that holds data. Very similar to how our containers in kitchen holds
+   sugar, salt etc Creating a variable is like creating a placeholder in memory and assigning it some value. In Python its as easy as writing:"""
+# a = 1
+# b = True
+# c = "Harry"
+# d = None
+
+#What is a Data Type?
+"""Data type specifies the type of value a variable holds. This is required in programming to do various operations without causing an error.
+In python, we can print the type of any operator using type function:"""
+# a = 1
+# print(type(a))
+# b = "1"
+# print(type(b))
+"""By default, python provides the following built-in data types:"""
+# int, flout, complex, str, boolean, list, tuple, dict
+
+### Operators ###
+"""Python has different types of operators for different operations. To create a calculator we require arithmetic operators."""
+
+#Arithmetic operators
+"""Operator	Operator Name	Example
+    +	    Addition	    15+7
+    -	    Subtraction	    15-7
+    *	    Multiplication	5*7
+    **	    Exponential	    5**3
+    /	    Division	    5/3
+    %	    Modulus	        15%7
+    //	    Floor Division	15//7      """
+
+### Typecasting in python ###
+"""The conversion of one data type into the other data type is known as type casting in python or type conversion in python.
+
+Python supports a wide variety of functions or methods like: int(), float(), str(), ord(), hex(), oct(), tuple(), set(), list(),
+dict(), etc. for the type casting in python."""
+
+## Two Types of Typecasting: ##
+
+""" Explicit Conversion (Explicit type casting in python)
+    Implicit Conversion (Implicit type casting in python) """
+# Explicit typecasting:
+"""The conversion of one data type into another data type, done via developer or programmer's intervention or manually as per the 
+requirement, is known as explicit type conversion.It can be achieved with the help of Python’s built-in type conversion functions 
+such as int(), float(), hex(), oct(), str(), etc ."""
+# Implicit type casting:
+"""Data types in Python do not have the same level i.e. ordering of data types is not the same in Python. Some of the data types have 
+higher-order, and some have lower order. While performing any operations on variables with different data types in Python, one of 
+the variable's data types will be changed to the higher data type. According to the level, one data type is converted into other 
+by the Python interpreter itself (automatically). This is called, implicit typecasting in python.Python converts a smaller data 
+type to a higher data type to prevent data loss."""
+
+### User Input ###
